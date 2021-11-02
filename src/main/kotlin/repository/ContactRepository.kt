cde440f26f888c983b37c6eee6ccfefacd1c8e58
nilsonsasaki <nilsonsasaki@gmail.com>
2021-11-02 13:54:55 -0300

    created ContactRepository.kt

---
+++ src/main/kotlin/repository/ContactRepository.kt
@@ -0,0 +1,24 @@
+package repository
+
+import entity.ContactEntity
+
+class ContactRepository {
+
+    companion object{
+
+        private val contactList = mutableListOf<ContactEntity>()
+
+        fun save(contact: ContactEntity){
+            contactList.add(contact)
+        }
+
+        fun delete(contact: ContactEntity){
+            contactList
+        }
+
+        fun getList(): List<ContactEntity>{
+            return contactList
+        }
+    }
+
+}
