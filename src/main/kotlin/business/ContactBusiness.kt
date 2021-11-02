5ab153602785545c1e711e322cc463b47944f208
nilsonsasaki <nilsonsasaki@gmail.com>
2021-11-02 13:51:48 -0300

    created ContactBusiness.kt

---
+++ src/main/kotlin/business/ContactBusiness.kt
@@ -0,0 +1,39 @@
+package business
+
+import entity.ContactEntity
+import repository.ContactRepository
+
+class ContactBusiness {
+    private fun validate(name: String, phone: String) {
+        if (name.isBlank()) {
+            throw Exception("Nome é obrigatório")
+        }
+        if (phone.isBlank()) {
+            throw Exception("Telefone é obrigatório")
+        }
+    }
+
+    private fun validateDelete(name: String, phone: String) {
+        if (name.isBlank() || phone.isBlank()) {
+            throw Exception("É necessário selecionar um contatoantes de remover")
+        }
+    }
+
+    fun save(name: String, phone: String) {
+
+        validate(name, phone)
+        val contact = ContactEntity(name, phone)
+        ContactRepository.save(contact)
+
+    }
+
+    fun delete(name: String, phone: String) {
+        validateDelete(name, phone)
+        val contact = ContactEntity(name, phone)
+        ContactRepository.delete(contact)
+    }
+
+    fun getList(): List<ContactEntity> {
+        return ContactRepository.getList()
+    }
+}
