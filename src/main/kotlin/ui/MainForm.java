979ca09754c18cd257a4f7cf94c30190208a03bb
nilsonsasaki <nilsonsasaki@gmail.com>
2021-11-02 13:56:36 -0300

    added button listeners

--- src/main/kotlin/ui/MainForm.java
+++ src/main/kotlin/ui/MainForm.java
@@ -1,13 +1,21 @@
 package ui;
 
+import business.ContactBusiness;
+import entity.ContactEntity;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
 public class MainForm extends JFrame{
     private JPanel rootPanel;
-    private JButton ButtonNewContact;
-    private JButton ButtonRemove;
-    private JTable Contacts;
+    private JButton buttonNewContact;
+    private JButton buttonRemove;
+    private JTable tableContacts;
+
+    private ContactBusiness mContactBusiness;
 
     public MainForm(){
 
@@ -19,5 +27,30 @@ public class MainForm extends JFrame{
         setLocation((dim.width - getSize().width)/2,(dim.height - getSize().height)/2);
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        mContactBusiness = new ContactBusiness();
+
+        setListeners();
+        loadContacts();
+    }
+
+    private void loadContacts() {
+       List<ContactEntity> contactList = mContactBusiness.getList();
+    }
+
+    private void setListeners(){
+        buttonNewContact.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                new ContactForm();
+                dispose();
+            }
+        });
+        buttonRemove.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
     }
 }
